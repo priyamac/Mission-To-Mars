7f5cce094eee3b56ea42eb79c7c2652865937ef9
priyamac <priya.mcnamara.pm@gmail.com>
2022-03-13 20:06:15 -0500

    Challenge

--- app.py
+++ app.py
@@ -8,15 +8,19 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
+@app.route("/")
+def index():
+   mars = mongo.db.mars.find_one()
+   return render_template("index.html", mars=mars)
+
 @app.route("/scrape")
 def scrape():
    mars = mongo.db.mars
    mars_data = scraping.scrape_all()
-   mars.update_one({}, {"$set":mars_data}, upsert=True)
-   return redirect('/', code=302)
+   # mars.update_one({}, {"$set":mars_data}, upsert=True)
+   #return redirect('/', code=302)
+   mars.update({}, mars_data, upsert=True)
+   return "Scraping Successful"
 
 if __name__ == "__main__":
    app.run()
-
-
-
